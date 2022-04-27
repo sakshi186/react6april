2b72aa2d2ce0c9b5fe208c6243f15e98c321de0c
sakshi186 <sakshisisodiya@gmail.com>
2022-04-27 13:57:48 +0530

    use state

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,4 @@
-import React,{useState} from 'react'
-import Home from './Home'
-import Contact from './Contact'
-import '../App.css'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import React from 'react'
 
 
 export default function App() {
@@ -14,12 +6,6 @@ export default function App() {
     //2.functions
     //3.return statements
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}/>
-        <Route index element={<Home />} />
-        <Route index element={<Contact />} />
-    </Routes>
-  </BrowserRouter>
+    
   )
 }
